b4582fbbe916e29b3ccb4d6b398896990fc24a6c
Tom Leger <tomleger@gmail.com>
2022-06-17 14:13:59 -0400

    first commit

---
+++ execution_time.rb
@@ -0,0 +1,88 @@
+def my_min(nums) # time = O(n^2)
+    nums.each_with_index do |num_1, index_1|
+        smaller = false
+        nums.each_with_index do |num_2, index_2|
+            if index_1 != index_2 
+                smaller = true if num_2 < num_1
+            end
+        end
+        return num_1 if !smaller
+    end
+end
+
+def my_min_two(nums) # time = O(n)
+    min = nums.first
+    nums.each { |num| min = num if num < min }
+    min
+end
+
+def my_min_three(nums) # time = O(n)
+    nums.inject { |acc, ele|  acc > ele ? acc = ele : acc } 
+end
+
+list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# p my_min(list)  # =>  -5
+
+# p my_min_two(list)  # =>  -5
+
+# p my_min_three(list)  # =>  -5
+
+# Write a function that iterates through the array and finds 
+# all sub-arrays using nested loops. 
+# First make an array to hold all sub-arrays. 
+# Then find the sums of each sub-array and return the max.
+
+
+# Discuss the time complexity of this solution.
+
+def largest_contiguous_subsum(nums) # time = O(n^2)
+    subsets = []
+    (0...nums.length).each do |i|
+        (0...nums.length).each do |j|
+            subsets << nums[i..j] if i <= j
+        end
+    end
+    subsetsums =[]
+    subsets.each do |ele|
+        subsetsums << ele.sum
+    end
+    subsetsums.max
+end
+
+def largest_contiguous_subsum_two(nums) # time = O(n)
+    sums = 0
+    nums.each { |num| sums += num if num > 0 }
+    sums
+end
+
+# Let's make a better version. 
+# Write a new function using O(n) time with O(1) memory. 
+# Keep a running tally of the largest sum. To accomplish this 
+# efficient space complexity, consider using two variables. 
+# One variable should track the largest sum so far and another 
+# to track the current sum. We'll leave the rest to you.
+
+def largest_contiguous_subsum_three(nums)
+    largest_sum = 0
+    current_sum = 0
+    nums.each do |num|
+        current_sum += num
+        current_sum > largest_sum ? largest_sum = current_sum : largest_sum
+    end
+
+    largest_sum
+    
+
+end
+
+list = [5, 3, -7]
+p largest_contiguous_subsum(list) # => 8
+p largest_contiguous_subsum_two(list) # => 8
+p largest_contiguous_subsum_three(list) # => 8
+    # possible sub-sums
+    # [5]           # => 5
+    # [5, 3]        # => 8 --> we want this one
+    # [5, 3, -7]    # => 1
+    # [3]           # => 3
+    # [3, -7]       # => -4
+    # [-7]          # => -7
